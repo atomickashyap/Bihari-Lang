cacdf06d583501e9b92f331082b2df031e7ca837
AmanKas <aman312000@gmail.com>
2023-02-16 17:59:11 +0530

    Token Construction

---
+++ basic.py
@@ -0,0 +1,20 @@
+################################
+# TOKENS
+################################
+TT_INT = 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
+TT_DIV = 'DIV'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+    
+    def __repr__(self):
+        if self.value: return f'{self.type}{self.value}'
+        return f'{self.type}'    
